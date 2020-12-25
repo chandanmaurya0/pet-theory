f9abec6e8af3ffb5018d3550d3e5329173071c86
chandanmaurya0 <49331380+chandanmaurya0@users.noreply.github.com>
2020-12-25 11:15:09 +0530

    Update index.js

--- lab03/index.js
+++ lab03/index.js
@@ -1,6 +1,10 @@
-const express    = require('express');
-const app        = express();
-const bodyParser = require('body-parser');
+const {promisify} = require('util');
+const {Storage}   = require('@google-cloud/storage');
+const exec        = promisify(require('child_process').exec);
+const storage     = new Storage();
+const express     = require('express');
+const bodyParser  = require('body-parser');
+const app         = express();
 
 app.use(bodyParser.json());
 
@@ -11,17 +15,45 @@ app.listen(port, () => {
 });
 
 app.post('/', async (req, res) => {
-  console.log('OK');
   try {
     const file = decodeBase64Json(req.body.message.data);
-    console.log(`file: ${JSON.stringify(file)}`);
+    await downloadFile(file.bucket, file.name);
+    const pdfFileName = await convertFile(file.name);
+    await uploadFile(process.env.PDF_BUCKET, pdfFileName);
+    await deleteFile(file.bucket, file.name);
   }
   catch (ex) {
-    console.log(ex);
+    console.log(`Error: ${ex}`);
   }
   res.set('Content-Type', 'text/plain');
   res.send('\n\nOK\n\n');
 })
+
+async function downloadFile(bucketName, fileName) {
+  const options = {destination: `/tmp/${fileName}`};
+  await storage.bucket(bucketName).file(fileName).download(options);
+}
+
+async function convertFile(fileName) {
+  const cmd = 'libreoffice --headless --convert-to pdf --outdir /tmp ' +
+              `"/tmp/${fileName}"`;
+  console.log(cmd);
+  const { stdout, stderr } = await exec(cmd);
+  if (stderr) {
+    throw stderr;
+  }
+  console.log(stdout);
+  pdfFileName = fileName.replace(/\.\w+$/, '.pdf');
+  return pdfFileName;
+}
+
+async function deleteFile(bucketName, fileName) {
+  await storage.bucket(bucketName).file(fileName).delete();
+}
+
+async function uploadFile(bucketName, fileName) {
+  await storage.bucket(bucketName).upload(`/tmp/${fileName}`);
+}
 
 function decodeBase64Json(data) {
   return JSON.parse(Buffer.from(data, 'base64').toString());
